e55ed4597f4905242b0c9188cb4ee9aaf9aa38e7
Kassia Leão <kassiadarcie@gmail.com>
2024-12-05 16:09:23 -0300

    criada classe Autor

---
+++ src/main/java/model/entities/Autor.java
@@ -0,0 +1,55 @@
+package model.entities;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Autor implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    private int codAutor;
+    private String nome;
+
+    public Autor(int codAutor, String nome) {
+        this.codAutor = codAutor;
+        this.nome = nome;
+    }
+
+    public int getCodAutor() {
+        return codAutor;
+    }
+
+    public void setCodAutor(int codAutor) {
+        this.codAutor = codAutor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Autor autor = (Autor) o;
+        return getCodAutor() == autor.getCodAutor();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(getCodAutor());
+    }
+
+    @Override
+    public String toString() {
+        return "Autor{" +
+                "codAutor=" + getCodAutor() +
+                ", nome='" + getNome() + '\'' +
+                '}';
+    }
+}
