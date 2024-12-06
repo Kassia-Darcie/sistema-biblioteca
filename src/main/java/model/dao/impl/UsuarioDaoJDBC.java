24591c9a6df78b8e64f82548515bc3c59b3c92b9
Kassia Leão <kassiadarcie@gmail.com>
2024-12-06 15:08:04 -0300

    implementado métodos update, deleteByCpf, findByCpf e findAll

--- src/main/java/model/dao/impl/UsuarioDaoJDBC.java
+++ src/main/java/model/dao/impl/UsuarioDaoJDBC.java
@@ -6,6 +6,7 @@ import model.dao.UsuarioDao;
 import model.entities.Usuario;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UsuarioDaoJDBC implements UsuarioDao {
@@ -41,26 +42,98 @@ public class UsuarioDaoJDBC implements UsuarioDao {
             }
         } catch (SQLException e) {
             throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatment(ps);
         }
     }
 
     @Override
     public void update(Usuario obj) {
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement("UPDATE usuario " +
+                    "SET Nome = ?, Email = ?, Telefone = ?, DataNasc = ?, Endereco = ? " +
+                    "WHERE CpfUsuario = ?", Statement.RETURN_GENERATED_KEYS);
 
+            ps.setString(1, obj.getNome());
+            ps.setString(2, obj.getEmail());
+            ps.setString(3, obj.getTelefone());
+            ps.setDate(4, Date.valueOf(obj.getDataNasc()));
+            ps.setString(5, obj.getEndereco());
+            ps.setString(6, obj.getCpf());
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatment(ps);
+        }
     }
 
     @Override
     public void deleteByCpf(String cpf) {
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement("DELETE FROM usuario WHERE CpfUsuario = ?");
+            ps.setString(1, cpf);
 
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatment(ps);
+        }
     }
 
     @Override
     public Usuario findByCpf(String cpf) {
-        return null;
+        Usuario usuario = new Usuario();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = conn.prepareStatement("SELECT * FROM usuario WHERE usuario.CpfUsuario = ?");
+            ps.setString(1, cpf);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                return instantiateUsuario(rs);
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatment(ps);
+            DB.closeResultSet(rs);
+        }
+    }
+
+    private static Usuario instantiateUsuario(ResultSet rs) throws SQLException {
+        Usuario usuario = new Usuario();
+        usuario.setCpf(rs.getString("CpfUsuario"));
+        usuario.setNome(rs.getString("Nome"));
+        usuario.setEmail(rs.getString("Email"));
+        usuario.setTelefone(rs.getString("Telefone"));
+        usuario.setDataNasc(rs.getDate("DataNasc").toLocalDate());
+        usuario.setEndereco(rs.getString("Endereco"));
+        return usuario;
     }
 
     @Override
     public List<Usuario> findAll() {
-        return List.of();
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = conn.prepareStatement("SELECT * FROM usuario ORDER BY Nome");
+            rs = ps.executeQuery();
+            List<Usuario> usuarios = new ArrayList<>();
+            while (rs.next()) {
+                usuarios.add(instantiateUsuario(rs));
+            }
+            return usuarios;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatment(ps);
+            DB.closeResultSet(rs);
+        }
     }
 }
