69b67fcccef9522b86d4007af83929cc2eb1c564
Kassia Leão <kassiadarcie@gmail.com>
2024-12-06 23:00:38 -0300

    adicionada funcionalidade de cadastrar um livro

---
+++ src/main/java/model/dao/impl/LivroDaoJDBC.java
@@ -0,0 +1,138 @@
+package model.dao.impl;
+
+import db.DB;
+import db.DbException;
+import model.dao.LivroDao;
+import model.dao.UsuarioDao;
+import model.entities.*;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LivroDaoJDBC implements LivroDao {
+    private final Connection conn;
+
+    public LivroDaoJDBC(Connection conn) {
+        this.conn = conn;
+    }
+
+    @Override
+    public void insert(Livro obj) {
+        PreparedStatement ps = null;
+
+        try {
+            ps = conn.prepareStatement("INSERT INTO livro (Titulo, Isbn, Edicao, AnoPub, CodCat, CodEditora)" +
+                    "VALUES (?, ?, ?, ?, ?, ?)", Statement.RETURN_GENERATED_KEYS);
+            ps.setString(1, obj.getTitulo());
+            ps.setString(2, obj.getIsbn());
+            ps.setInt(3, obj.getEdicao());
+            ps.setInt(4, obj.getAnoPub());
+            ps.setInt(5, obj.getCategoria().getCodCat());
+            ps.setInt(6, obj.getEditora().getCodEditora());
+
+            int rowsAffected = ps.executeUpdate();
+            if (rowsAffected > 0) {
+                ResultSet rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    int id = rs.getInt(1);
+                    obj.setCodLivro(id);
+                    DB.closeResultSet(rs);
+                }
+                for (Autor autor: obj.getAutor()) {
+                    ps = conn.prepareStatement("INSERT INTO escrito (CodLivro, CodAutor) VALUES (?, ?)");
+                    ps.setInt(1, obj.getCodLivro());
+                    ps.setInt(2, autor.getCodAutor());
+                    ps.executeUpdate();
+                    System.out.println(autor);
+                }
+            } else {
+                throw new DbException("Erro ao inserir livro");
+            }
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatment(ps);
+
+        }
+    }
+
+    @Override
+    public void update(Livro obj) {
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement("UPDATE livro " +
+                    "SET Titulo = ?, Isbn = ?, Edicao = ?, AnoPub = ?, CodCat = ?, CodEditora = ? " +
+                    "WHERE CodLivro = ?");
+
+            ps.setString(1, obj.getTitulo());
+            ps.setString(2, obj.getIsbn());
+            ps.setInt(3, obj.getEdicao());
+            ps.setInt(4, obj.getAnoPub());
+            ps.setInt(5, obj.getCategoria().getCodCat());
+            ps.setInt(6, obj.getEditora().getCodEditora());
+            ps.setInt(7, obj.getCodLivro());
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatment(ps);
+        }
+    }
+
+    @Override
+    public void deleteById(int id) {
+        PreparedStatement ps = null;
+        try {
+            ps = conn.prepareStatement("DELETE FROM livro WHERE CodLivro = ?");
+            ps.setInt(1, id);
+
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatment(ps);
+        }
+    }
+
+    @Override
+    public Livro findById(int id) {
+        /*PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            ps = conn.prepareStatement("SELECT livro.*, FROM usuario WHERE usuario.CpfUsuario = ?");
+            ps.setString(1, cpf);
+            rs = ps.executeQuery();
+            if (rs.next()) {
+                return instantiateUsuario(rs);
+            }
+            return null;
+        } catch (SQLException e) {
+            throw new DbException(e.getMessage());
+        } finally {
+            DB.closeStatment(ps);
+            DB.closeResultSet(rs);
+        }*/
+        return null;
+    }
+
+    @Override
+    public List<Livro> findAll() {
+        return List.of();
+    }
+
+    private static Livro instantiateLivro(ResultSet rs, Categoria cat, Editora editora) throws SQLException {
+        Livro livro = new Livro();
+        livro.setCodLivro(rs.getInt("CodLivro"));
+        livro.setTitulo(rs.getString("Titulo"));
+        livro.setIsbn(rs.getString("Isbn"));
+        livro.setEdicao(rs.getInt("Edicao"));
+        livro.setAnoPub(rs.getInt("AnoPub"));
+        livro.setCategoria(cat);
+        livro.setEditora(editora);
+        return livro;
+    }
+
+
+}
--- src/main/java/model/entities/Biblioteca.java
+++ src/main/java/model/entities/Biblioteca.java
@@ -1,15 +1,17 @@
 package model.entities;
 
-import model.dao.DaoFactory;
-import model.dao.UsuarioDao;
+import model.dao.*;
 
 import java.time.LocalDate;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Biblioteca {
     private UsuarioDao usuarioDao = DaoFactory.createUsuarioDao();
+    private CategoriaDao categoriaDao = DaoFactory.createCategoriaDao();
+    private EditoraDao editoraDao = DaoFactory.createEditoraDao();
+    private AutorDao autorDao = DaoFactory.createAutorDao();
+    private LivroDao livroDao = DaoFactory.createLivroDao();
 
     public void registerUsuario() {
         Scanner sc = new Scanner(System.in);
@@ -33,10 +35,46 @@ public class Biblioteca {
         sc.close();
     }
 
+    public void updateUsuario(String cpf, String nome, String email, String telefone, LocalDate nascimento, String endereco) {
+        Usuario usuario = usuarioDao.findByCpf(cpf);
+        usuario.setNome(nome);
+        usuario.setEmail(email);
+        usuario.setTelefone(telefone);
+        usuario.setDataNasc(nascimento);
+        usuario.setEndereco(endereco);
+        usuarioDao.update(usuario);
+    }
+
     public void listAllUser() {
         List<Usuario> usuarios = usuarioDao.findAll();
         for (Usuario usuario : usuarios) {
             System.out.println(usuario);
         }
+    }
+
+    public void registerLivro (Livro livro) {
+        Categoria categoria = categoriaDao.findByNome(livro.getCategoria().getNome());
+        Editora editora = editoraDao.findByNome(livro.getEditora().getNome());
+
+
+        if (categoria == null) {
+            categoriaDao.insert(livro.getCategoria());
+        } else {
+            livro.setCategoria(categoria);
+        }
+        if (editora == null) {
+            editoraDao.insert(livro.getEditora());
+        } else {
+            livro.setEditora(editora);
+        }
+        for (Autor a: livro.getAutor()) {
+            Autor autor = autorDao.findByNome(a.getNome());
+            if (autor == null) {
+                autorDao.insert(a);
+            } else {
+                a.setCodAutor(autor.getCodAutor());
+            }
+        }
+        livroDao.insert(livro);
     }
 }
