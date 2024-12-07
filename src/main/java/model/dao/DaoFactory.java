74237d82e021c9ced38b55800783b851f8364764
Kassia Leão <kassiadarcie@gmail.com>
2024-12-06 22:58:58 -0300

    atualizado DaoFactory

--- src/main/java/model/dao/DaoFactory.java
+++ src/main/java/model/dao/DaoFactory.java
@@ -1,10 +1,26 @@
 package model.dao;
 
 import db.DB;
-import model.dao.impl.UsuarioDaoJDBC;
+import model.dao.impl.*;
 
 public class DaoFactory {
     public static UsuarioDao createUsuarioDao() {
         return new UsuarioDaoJDBC(DB.getConnection());
+    }
+
+    public static LivroDao createLivroDao() {
+        return new LivroDaoJDBC(DB.getConnection());
+    }
+
+    public static CategoriaDao createCategoriaDao() {
+        return new CategoriaDaoJDBC(DB.getConnection());
+    }
+
+    public static AutorDao createAutorDao() {
+        return new AutorDaoJDBC(DB.getConnection());
+    }
+
+    public static EditoraDao createEditoraDao() {
+        return new EditoraDaoJDBC(DB.getConnection());
     }
 }
